9873c6b5cddff18667f84543cb69e80b285cc1ad
Ritz6533 <riteshjungshah6533@gmail.com>
2023-05-21 22:17:36 +0100

    added final container about page

--- wuc/public/pages/About Us.html.php
+++ wuc/public/pages/About Us.html.php
@@ -35,12 +35,36 @@ require '../layouttempelate/layout.html.php';
 
     <div class="abtcontainer3">
   <div class="image-section">
-    <img src="your-image.jpg" alt="Image">
+  <img src="../image/person.jpg" alt="Image" >
   </div>
   <div class="text-section">
-    <p>Your text goes here</p>
+    <h2> Mr. Simones Johns
+    </h2>
+    <p>There are many variations of passages of Lorem Ipsum available,
+     but the majority have suffered alteration in some form, by injected humour,
+     There are many variations of passages of Lorem Ipsum available,
+     but the majority have suffered alteration in some form, by injected humour,<br><br>
+     There are many variations of passages of Lorem Ipsum available,
+     but the majority have suffered alteration in some form, by injected humour,
+     There are many variations of passages of Lorem Ipsum available,
+     but the majority have suffered alteration in some form, by injected humour,</p>
   </div>
 </div>
+
+<div class="outer-container">
+  <div class="inner-container">
+  <h2> STUDENT SUPPORT</h2>
+  </div>
+  <div class="inner-container">
+  <h2> FACULTY MEMBER</h2>
+
+  </div>
+  <div class="inner-container">
+  <h2> ADMISSION SUPPORT</h2>
+
+  </div>
+</div>
+
 
 
 </main>
