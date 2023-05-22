d80054a108f469ee3b418b5338c3cf63b69d39ca
Ritz6533 <riteshjungshah6533@gmail.com>
2023-05-22 01:07:00 +0100

    add studentlife content

--- wuc/public/pages/Student Life.html.php
+++ wuc/public/pages/Student Life.html.php
@@ -6,8 +6,57 @@ require '../layouttempelate/layout.html.php';
 ?>
 
 <main class="mainclass">
-    <h2>Student Life</h2>
-    <p>Welcome to the Internet job Database. Here are some latest jobs in the UK</p>
+
+<div class="stdpagecontainer1">
+
+<img class= "containerimg"src="../image/student.jpg" alt="Background Image">
+<div class="container1textstdpage">
+<h2>Student Life And Events</h2>
+</div>
+</div>
+
+<div class="scontainer">
+<div class="studentcontainer">
+
+  <div class="image-container">
+  <img src="../image/contact.jpg" alt="Image" >
+      <div class="image-text">EVENTS</div>
+  </div>
+  <div class="image-container">
+  <img src="../image/contact.jpg" alt="Image" >
+      <div class="image-text">STUDENT PERKS</div>
+  </div>
+  <div class="image-container">
+  <img src="../image/contact.jpg" alt="Image" >
+      <div class="image-text">BLOGS</div>
+  </div></div>
+
+  <div class="bottom-txt">
+    Enhancing Your Student Life Experience
+  </div>
+</div>
+
+<div class="stdcontainer">
+ 
+  <div class="text-section">
+    <h3>Student Life</h3>
+    <p> I must explain to you how all this mistaken idea of denouncing pleasure
+         and praising pain was born and I will give you a complete account of the
+          system, and expound the actual teachings of the great explorer of the truth,
+           the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure
+            itself, because it is pleasure, but because those who do not know how to pursue pleasure
+             rationally encounter consequences that are extremely painful. Nor again is there anyone who 
+             loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally </p>
+
+  </div>
+  <div class="image-section1">
+  <img src="../image/studentlife.jpg" alt="Image" >
+  </div>
+</div>
+
+<div class="bar">
+  </div>
+
 </main>
 
 <?php require '../layouttempelate/footer.html.php'; ?>
